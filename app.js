c6cd1688b44e18c21ec26275be86b2c82bec1e47
muhammadazeemkhan <121564168+muhammadazeemkhan@users.noreply.github.com>
2023-05-06 17:49:01 +0500

    Add files via upload
    completed

---
+++ app.js
@@ -0,0 +1,52 @@
+function question_1(){
+    var userName = "Muhammad Azeem Khan"
+    alert(userName)
+}
+
+function question_2(){
+    var myName = prompt("Enter Your full name")
+    alert("Your Full Name is " +  myName)
+}
+
+
+function question_3(){
+    var messege = "Hello World"
+    alert(messege)
+}
+
+
+function question_4(){
+    var studentName = "Jhone doe"
+    var  age = "15 years old"
+    var course = "Certified Mobile Application Development"
+    alert(studentName);
+    alert(age);
+    alert(course)
+
+}
+
+
+function question_5(){
+    alert("Pizza" + "\n" + "Pizz" + "\n" + "Piz" + "\n" + "Pi" + "\n" + "Pi")
+}
+
+
+function question_6(){
+    var email = "azeemhps@gmail.com"
+    alert("My Email adress is " + email)
+}
+
+function question_7(){
+    var book = "A smarter way to learn JavaScript"
+    alert("I am trying to learn from the book " + book)
+}
+
+
+function question_8(){
+    document.write("Yah! I can write HTML content  through javascript")
+}
+
+function question_9(){
+    var pattern = "▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬"
+    alert(pattern)
+}
